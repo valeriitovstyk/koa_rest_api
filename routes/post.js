6ede209628836efe8eeadb796ab5065e9a9e1c36
valeriitovstyk <valeriitovstyk@gmail.com>
2019-10-24 23:40:59 +0300

    koa-jwt, start for jwt authentication

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const PostController = require('../controllers/PostController');
 const koaBody = require('koa-body');
 // const models = require('../models/index');
+const jwt = require('../middleware/jwt');
 
 const router = new Router();
 router
@@ -11,8 +12,8 @@ router
     .get('/author/:id', PostController.getPostListByAuthorId)
      // doesn't properly work with name parameter
     // .get('/author/:name', PostController.getPostListByAuthorName)
-    .post('/', koaBody(), PostController.addPost)
-    .put('/:id', koaBody(), PostController.updatePost)
-    .delete('/:id', PostController.removePost);
+    .post('/', jwt, koaBody(), PostController.addPost)
+    .put('/:id', jwt, koaBody(), PostController.updatePost)
+    .delete('/:id', jwt, PostController.removePost);
 
 module.exports = router.routes();
