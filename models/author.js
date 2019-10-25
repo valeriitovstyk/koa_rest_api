429a5ca1cadd1e3b92f4bdbbeeed865ad85113f9
valeriitovstyk <valeriitovstyk@gmail.com>
2019-10-25 06:19:06 +0300

    add id field to models, moved out faker and sync

--- models/author.js
+++ models/author.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
 const Author = sequelize.define('author', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     name: {
         type: Sequelize.STRING
     },
--- models/post.js
+++ models/post.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
 const Post = sequelize.define('post', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     title: {
         type: Sequelize.STRING
     },
